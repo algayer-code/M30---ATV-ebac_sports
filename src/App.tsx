3c4630445c9d6561aa6a5a8f257b0064cad73ac9
Algayer <nairjoaocarlos@gmail.com>
2025-11-10 17:09:58 -0300

    teste

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ function App() {
 
   function adicionarAoCarrinho(produto: Produto) {
     if (carrinho.find((p) => p.id === produto.id)) {
-      alert('Item já adicionado')
+      alert('Items já adicionado')
     } else {
       setCarrinho([...carrinho, produto])
     }
